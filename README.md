74f0dd853f256d59a54063f4e1dad7540122d1d2
dune6 <ilya.voznesensky@gmail.com>
2025-03-14 09:34:56 +0300

    Init proj

---
+++ README.md
@@ -0,0 +1,6 @@
+# workout_manager_client
+
+Workout manager client for android, ios, web platforms.
+
+## Getting Started
+
---
+++ android/app/src/main/kotlin/com/dune6/workout_manager_client/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.dune6.workout_manager_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
